5b1fadb90907503a8704257976b5291270d656c7
chincharovpc <chincharovpc@yandex.ru>
2024-08-12 16:13:44 +0300

    Исправление ошибок

--- auth/transport/api/request/auth.go
+++ auth/transport/api/request/auth.go
@@ -12,7 +12,7 @@ type AuthRefreshRequest struct {
 }
 
 type AuthLogoutRequest struct {
-	Token string `json:"token" binding:"required"`
+	Token string `json:"accessToken" binding:"required"`
 }
 
 type AuthUpdateAccountDataRequest struct {
