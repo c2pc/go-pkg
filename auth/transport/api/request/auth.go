9be77a1bb34db76c585e5d5699b168a840886b44
chincharov <chincharovpc@yandex.ru>
2024-09-09 16:43:04 +0300

    Исправление ошибок

--- auth/transport/api/request/auth.go
+++ auth/transport/api/request/auth.go
@@ -20,7 +20,7 @@ type AuthUpdateAccountDataRequest struct {
 	FirstName  *string `json:"first_name" binding:"omitempty,max=255,min=2,dot_underscore_hyphen"`
 	SecondName *string `json:"second_name" binding:"omitempty,len=0|min=2,max=255,dot_underscore_hyphen"`
 	LastName   *string `json:"last_name" binding:"omitempty,len=0|min=2,max=255,dot_underscore_hyphen"`
-	Password   *string `json:"password" binding:"omitempty,max=255,min=8"`
+	Password   *string `json:"password" binding:"omitempty,max=255,min=8,spec_chars"`
 	Email      *string `json:"email" binding:"omitempty,len=0|email,max=255"`
 	Phone      *string `json:"phone" binding:"omitempty,len=0|min=1,max=255"`
 }
