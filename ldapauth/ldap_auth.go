ab4b6765cb8aafd4e8f67e2bee6d4d95e5a45907
Daniil <daniil.b@flat-soft.ru>
2025-01-30 18:48:28 +0300

    доменная авторизация

---
+++ ldapauth/ldap_auth.go
@@ -0,0 +1,18 @@
+package ldapauth
+
+import (
+	"github.com/c2pc/go-pkg/ldapauth/internal/config"
+	"github.com/c2pc/go-pkg/ldapauth/internal/service"
+)
+
+type AuthService interface {
+	Login(username, password string) (*TokenResponse, error)
+	Refresh(refreshToken string) (*TokenResponse, error)
+}
+
+type Config = config.Config
+type TokenResponse = service.TokenResponse
+
+func NewAuthService(cfg Config) AuthService {
+	return service.NewAuthService(cfg)
+}
