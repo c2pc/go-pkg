873f7461fb715e5c29fb06730694c9409fbc7e0e
ChincharovPC <57862647+c2pc@users.noreply.github.com>
2025-02-05 12:55:19 +0300

    Исправление пакета retry

--- utils/retry/retry.go
+++ utils/retry/retry.go
@@ -8,15 +8,17 @@ import (
 
 var ErrMaxAttempts = fmt.Errorf("max attempts exceeded error")
 
-func Retry(fn func() error, needRetry func(error) bool, delays []time.Duration) error {
-	for attempts := 0; attempts < len(delays); attempts++ {
+var DefaultDelays = []time.Duration{2 * time.Second}
+
+func Retry(fn func() error, needRetry func(error) bool, delay time.Duration, maxAttempts int) error {
+	for attempts := 0; attempts < maxAttempts; attempts++ {
 		err := fn()
 		if err == nil {
 			return nil
 		}
 		if needRetry(err) {
-			if attempts < len(delays)-1 {
-				time.Sleep(delays[attempts])
+			if attempts < maxAttempts {
+				time.Sleep(delay)
 				continue
 			}
 			return errors.Join(ErrMaxAttempts, err)
