6d0f65100ba0bdddad1fbbd907538edf418764d7
d.chernov <d.chernov@flat-soft.ru>
2025-03-25 11:16:29 +0300

    Добавлен пробел в допустимые символы для названия фильтров

--- auth/internal/transport/api/request/filter.go
+++ auth/internal/transport/api/request/filter.go
@@ -1,12 +1,12 @@
 package request
 
 type FilterCreateRequest struct {
-	Name     string `json:"name" binding:"required,max=255,min=2,dot_underscore_hyphen"`
+	Name     string `json:"name" binding:"required,max=255,min=2,dot_underscore_hyphen_space"`
 	Endpoint string `json:"endpoint" binding:"required,max=255,min=2"`
 	Value    string `json:"value" binding:"required,min=1"`
 }
 
 type FilterUpdateRequest struct {
-	Name  *string `json:"name" binding:"omitempty,max=255,min=2,dot_underscore_hyphen"`
+	Name  *string `json:"name" binding:"omitempty,max=255,min=2,dot_underscore_hyphen_space"`
 	Value *string `json:"value" binding:"omitempty,min=1"`
 }
