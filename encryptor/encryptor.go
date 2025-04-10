dc3b86820a594a71844faadd44c6dd042b63d3f6
c2pc <chincharovpc@yandex.ru>
2025-04-11 00:15:48 +0300

    Исправлен encryptor

--- encryptor/encryptor.go
+++ encryptor/encryptor.go
@@ -9,18 +9,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var plaintext string
+
 func Command() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "encrypt",
+		Use:     "encrypt 'string to encrypt'",
 		Short:   "Text Encryptor",
-		Example: "encrypt 'qwerty'",
+		Example: `"encrypt -t="qwerty"`,
 		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 0 {
-				cmd.Usage()
-				os.Exit(1)
+			if len(plaintext) == 0 {
+				if len(args) == 0 {
+					cmd.Usage()
+					os.Exit(1)
+				} else {
+					plaintext = strings.Join(args, " ")
+				}
 			}
-
-			plaintext := strings.Join(args, " ")
 
 			en, err := cipher.RC4Cipher.Encrypt([]byte(plaintext))
 			if err != nil {
@@ -29,9 +33,11 @@ func Command() *cobra.Command {
 			}
 
 			fmt.Printf("Plain Text: %s\n", plaintext)
-			fmt.Printf("Encrypted: %s\n", string(en))
+			fmt.Printf("Encrypted : %s\n", string(en))
 		},
 	}
+
+	cmd.Flags().StringVarP(&plaintext, "text", "t", "", "Text to encrypt")
 
 	return cmd
 }
