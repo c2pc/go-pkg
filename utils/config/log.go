a90dfb4b284475db570e28f4cae09ebeaee66e8d
c2pc <chincharovpc@yandex.ru>
2025-06-09 13:56:14 +0300

    Исправлена директория логов

--- utils/config/log.go
+++ utils/config/log.go
@@ -2,7 +2,7 @@ package config
 
 type LOG struct {
 	Debug      string `yaml:"debug,omitempty"`
-	Folder     string `yaml:"folder"`
+	Directory  string `yaml:"directory"`
 	MaxSize    int    `yaml:"max_size"`
 	MaxBackups int    `yaml:"max_backups"`
 	MaxAge     int    `yaml:"max_age"`
