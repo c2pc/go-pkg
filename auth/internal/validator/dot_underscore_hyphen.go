170edc8a080df9a01dc8b0962497b6413d77fe19
d.chernov <d.chernov@flat-soft.ru>
2025-09-25 12:35:41 +0300

    GOL-781: Добавлены запятые в валидатор для имен

--- auth/internal/validator/dot_underscore_hyphen.go
+++ auth/internal/validator/dot_underscore_hyphen.go
@@ -16,7 +16,7 @@ var DotUnderscoreHyphen validator.Func = func(fl validator.FieldLevel) bool {
 	if kind == reflect.String {
 		s := field.String()
 
-		isValid := regexp.MustCompile(`^[a-zA-Z0-9а-яА-ЯёЁ_.!@-]*$`).MatchString
+		isValid := regexp.MustCompile(`^[a-zA-Z0-9а-яА-ЯёЁ_.,!@-]*$`).MatchString
 
 		return isValid(s)
 	} else {
@@ -26,6 +26,6 @@ var DotUnderscoreHyphen validator.Func = func(fl validator.FieldLevel) bool {
 
 func DotUnderscoreHyphenValidation(v *validator.Validate) {
 	_ = v.RegisterValidation("dot_underscore_hyphen", DotUnderscoreHyphen, false)
-	_ = v.RegisterTranslation(translator.RegisterValidatorTranslation(translator.RU, "dot_underscore_hyphen", "{0} должен содержать только символы a-zA-Z0-9а-яА-ЯёЁ_.!@-", true))
-	_ = v.RegisterTranslation(translator.RegisterValidatorTranslation(translator.EN, "dot_underscore_hyphen", "{0} must contain only characters a-zA-Z0-9а-яА-ЯёЁ_.!@-", true))
+	_ = v.RegisterTranslation(translator.RegisterValidatorTranslation(translator.RU, "dot_underscore_hyphen", "{0} должен содержать только символы a-zA-Z0-9а-яА-ЯёЁ_.,!@-", true))
+	_ = v.RegisterTranslation(translator.RegisterValidatorTranslation(translator.EN, "dot_underscore_hyphen", "{0} must contain only characters a-zA-Z0-9а-яА-ЯёЁ_.,!@-", true))
 }
