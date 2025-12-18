82a35153c5535a348bfa2f8456c7b218fe78d2e0
c2pc <chincharovpc@yandex.ru>
2025-12-18 22:52:27 +0300

    Исправление EnsureAutoIncrement

--- utils/db/secuence.go
+++ utils/db/secuence.go
@@ -31,7 +31,7 @@ func EnsureAutoIncrement(db *gorm.DB, table, column string, start int) error {
 			return fmt.Errorf("get last_value: %w", err)
 		}
 
-		if lastVal <= int64(start) {
+		if lastVal < int64(start) {
 			if err := db.Exec(`ALTER SEQUENCE ` + seqName.String + ` RESTART WITH ` + strconv.Itoa(start)).Error; err != nil {
 				return fmt.Errorf("restart sequence: %w", err)
 			}
@@ -46,7 +46,7 @@ func EnsureAutoIncrement(db *gorm.DB, table, column string, start int) error {
 			return fmt.Errorf("get AUTO_INCREMENT: %w", err)
 		}
 
-		if autoInc.Valid && autoInc.Int64 <= int64(start) {
+		if autoInc.Valid && autoInc.Int64 < int64(start) {
 			if err := db.Exec(`ALTER TABLE ` + table + ` AUTO_INCREMENT = ` + strconv.Itoa(start)).Error; err != nil {
 				return fmt.Errorf("reset AUTO_INCREMENT: %w", err)
 			}
